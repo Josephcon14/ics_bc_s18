3f0d284d95253d4c2a0f9017f63fe7008ceea9e8
josephcon14 <josephcontreras10@gmail.com>
2018-06-15 15:35:30 -0700

    1stdrft

---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what is your first name?'
+first_name = gets.chomp
+puts 'Pleasure meeting you ' + first_name + ', what is your middle name?'
+middle_name =  gets.chomp
+puts 'And what may your last name be?'
+last_name =  gets.chomp 
+puts 'Again, pleasure meeting you ' + first_name + ' ' + middle_name + ' ' + last_name + '.'
