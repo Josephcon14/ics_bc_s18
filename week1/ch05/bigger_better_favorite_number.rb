ef5e55de4cf198633a27ffdf8f2c6b722b79da52
josephcon14 <josephcontreras10@gmail.com>
2018-06-15 17:41:25 -0700

    seconddraft

--- week1/ch05/bigger_better_favorite_number.rb
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -1,4 +1,4 @@
  puts 'Hello, what is your favorite number?'
- favorite_number= gets.chomp.to_f
+ favorite_number = gets.chomp.to_f
  better_number = (favorite_number+1).to_s 
  puts 'Good choice, however, wouldn\'t ' + better_number + ' be a better option?'
